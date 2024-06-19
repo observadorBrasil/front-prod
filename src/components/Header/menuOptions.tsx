72565ed32f2169c6141269657070086c07644edb
IsaqueCruzDev <isaquedeoliveiracruz@outlook.com>
2024-06-19 20:28:39 -0300

    feat: go to atores

--- src/components/Header/menuOptions.tsx
+++ src/components/Header/menuOptions.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { IoMdSettings } from 'react-icons/io'
 import { IoLogOut } from 'react-icons/io5'
+import { TbWorldShare } from "react-icons/tb";
 
 export const menuOptions = [
   {
     label: 'Configurações',
     route: '/profile',
     icon: <IoMdSettings />,
+  },
+  {
+    label: 'Atores',
+    route: 'https://atores.azurewebsites.net/login',
+    icon: <TbWorldShare />,
   },
   {
     label: 'Sair',
